e10ea99c93625770d1756ad5d0125449a380daed
josephMasu <josephmuhindo089@gmail.com>
2025-05-08 01:10:41 +0300

    error handling

---
+++ src/error.rs
@@ -0,0 +1,113 @@
+use serde::{Serialize, Deserialize};
+use std::fmt;
+use axum::{http::StatusCode, response::IntoResponse, Json};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ErrorResponce{
+    pub status: String,
+    pub message: String,
+}
+
+impl fmt::Display for ErrorResponce {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", serde_json::to_string(&self).unwrap())
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub enum ErrorMessage {
+    EmptyPassword,
+    ExceededMaxPasswordLength(usize),
+    InvalidHashFormat,
+    HashingError,
+    InvalidToken,
+    WrongCredentials,
+    EmailExist,
+    UserNoLongerExist,
+    TokenNotProvided,
+}
+
+impl ToString for ErrorMessage {
+    fn to_string(&self) -> String {
+        self.to_str().to_owned()
+    }
+}
+
+impl ErrorMessage {
+    fn to_str(&self) -> String {
+        match self {
+            ErrorMessage::WrongCredentials => "Email or password is wrong".to_string(),
+            ErrorMessage::EmailExist => "A user with this email already exists".to_string(),
+            ErrorMessage::UserNoLongerExist => "User belonging to this token no longer exists".to_string(),
+            ErrorMessage::EmptyPassword => "Password cannot be empty".to_string(),
+            ErrorMessage::HashingError => "Error while hashing password".to_string(),
+            ErrorMessage::InvalidHashFormat => "Invalid password hash format".to_string(),
+            ErrorMessage::ExceededMaxPasswordLength(max_length) => format!("Password must not be more than {} characters", max_length),
+            ErrorMessage::InvalidToken => "Authentication token is invalid or expired".to_string(),
+            ErrorMessage::TokenNotProvided => "You are not logged in, please provide a token".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct HttpError{
+    pub message: String,
+    pub status: StatusCode,
+}
+ impl HttpError {
+    pub fn new(message: impl Into<String>, status: StatusCode) -> Self {
+        HttpError { 
+            message:message.into(),
+            status }
+    }
+    pub fn server_error(message: impl Into<String>) -> Self {
+        HttpError{
+            message: message.into(),
+            status: StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+    pub fn bad_request(message: impl Into<String>) -> Self {
+        HttpError{
+            message: message.into(),
+            status: StatusCode::BAD_REQUEST,
+        }
+    }
+    pub fn unique_contraint_violation(message: impl Into<String>) -> Self {
+        HttpError{
+            message: message.into(),
+            status: StatusCode::CONFLICT,
+        }
+    }
+    pub fn unauthorized(message: impl Into<String>) -> Self {
+        HttpError{
+            message: message.into(),
+            status: StatusCode::UNAUTHORIZED,
+        }
+    }
+    pub fn into_http_responce(self) -> axum::response::Response{
+        let json_response = Json(ErrorResponce{
+            status: "fail".to_string(),
+            message: self.message,
+        
+        });
+        (self.status, json_response).into_response().into()
+    }
+}
+
+impl fmt::Display for HttpError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "HttpError: message: {}, status: {}",
+            self.message, self.status
+        )
+    }
+}
+ 
+impl std::error::Error for HttpError {}
+
+impl IntoResponse for HttpError{
+    fn into_response(self) -> axum::response::Response {
+        self.into_http_responce()
+    }
+}
